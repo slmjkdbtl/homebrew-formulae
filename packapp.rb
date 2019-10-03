5c35b125535ec8fbd891963ef98981e8829f5735
Tiga Wu <tga@wengwengweng.me>
2019-10-03 19:33:31 -0400

    remove linux sections

--- packapp.rb
+++ packapp.rb
@@ -5,13 +5,8 @@ class Packapp < Formula
 	desc "pack a binary to MacOS .app bundle"
 	homepage "https://github.com/slmjkdbtl/packapp"
 	version 'v0.0.0'
-
-	if OS.mac?
-		url "https://github.com/slmjkdbtl/packapp/releases/download/#{version}/packapp-x86_64-apple-darwin.zip"
-		sha256 "f54d677b4604ab968008cfd6e43fb9225af8dda3e78ce92b608298d0de279b57"
-	elsif OS.linux?
-		url "https://github.com/slmjkdbtl/packapp/releases/download/#{version}/packapp-x86_64-unknown-linux-gnu.zip"
-	end
+	url "https://github.com/slmjkdbtl/packapp/releases/download/#{version}/packapp-x86_64-apple-darwin.zip"
+	sha256 "f54d677b4604ab968008cfd6e43fb9225af8dda3e78ce92b608298d0de279b57"
 
 	def install
 		bin.install "packapp"
--- ydb.rb
+++ ydb.rb
@@ -5,13 +5,8 @@ class Ydb < Formula
 	desc "yugioh database"
 	homepage "https://github.com/slmjkdbtl/ydb"
 	version 'v0.0.0'
-
-	if OS.mac?
-		url "https://github.com/slmjkdbtl/ydb/releases/download/#{version}/ydb-x86_64-apple-darwin.zip"
-		sha256 "08e9c9184c93fa72956e4ccd8de5e77a0df56a2d27e755e979420fa4cd778dec"
-	elsif OS.linux?
-		url "https://github.com/slmjkdbtl/ydb/releases/download/#{version}/ydb-x86_64-unknown-linux-gnu.zip"
-	end
+	url "https://github.com/slmjkdbtl/ydb/releases/download/#{version}/ydb-x86_64-apple-darwin.zip"
+	sha256 "08e9c9184c93fa72956e4ccd8de5e77a0df56a2d27e755e979420fa4cd778dec"
 
 	def install
 		bin.install "ydb"
