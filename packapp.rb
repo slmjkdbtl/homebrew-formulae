a0195b41885be7714078831f4ce67e3138896bdf
Tiga Wu <tga@wengwengweng.me>
2019-10-03 13:04:50 -0400

    add sha256s

--- packapp.rb
+++ packapp.rb
@@ -8,6 +8,7 @@ class Packapp < Formula
 
 	if OS.mac?
 		url "https://github.com/slmjkdbtl/packapp/releases/download/#{version}/packapp-x86_64-apple-darwin.zip"
+		sha256 "f54d677b4604ab968008cfd6e43fb9225af8dda3e78ce92b608298d0de279b57"
 	elsif OS.linux?
 		url "https://github.com/slmjkdbtl/packapp/releases/download/#{version}/packapp-x86_64-unknown-linux-gnu.zip"
 	end
--- ydb.rb
+++ ydb.rb
@@ -8,6 +8,7 @@ class Ydb < Formula
 
 	if OS.mac?
 		url "https://github.com/slmjkdbtl/ydb/releases/download/#{version}/ydb-x86_64-apple-darwin.zip"
+		sha256 "08e9c9184c93fa72956e4ccd8de5e77a0df56a2d27e755e979420fa4cd778dec"
 	elsif OS.linux?
 		url "https://github.com/slmjkdbtl/ydb/releases/download/#{version}/ydb-x86_64-unknown-linux-gnu.zip"
 	end
