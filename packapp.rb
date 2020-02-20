6df82c89a4f00c2ad797321898b099fe0cf9ccff
Tiga Wu <tga@wengwengweng.me>
2020-02-20 11:26:42 -0500

    update packapp

--- packapp.rb
+++ packapp.rb
@@ -6,7 +6,7 @@ class Packapp < Formula
 	homepage "https://github.com/slmjkdbtl/packapp"
 	version 'v0.0.0'
 	url "https://github.com/slmjkdbtl/packapp/releases/download/#{version}/packapp-x86_64-apple-darwin.zip"
-	sha256 "f54d677b4604ab968008cfd6e43fb9225af8dda3e78ce92b608298d0de279b57"
+	sha256 "d254ff9a334741054147d4748d91371d0f392bcdb334c2da6dbad71f8cddf521"
 
 	def install
 		bin.install "packapp"
