b84a64c966ab321c977126526fea8e2555807b99
Tiga Wu <tga@wengwengweng.me>
2020-04-15 00:20:59 -0400

    remove v

--- packapp.rb
+++ packapp.rb
@@ -4,7 +4,7 @@ class Packapp < Formula
 
 	desc "pack a binary to MacOS .app bundle"
 	homepage "https://github.com/slmjkdbtl/packapp"
-	version "v0.0.0"
+	version "0.0.0"
 	url "https://github.com/slmjkdbtl/packapp/releases/download/#{version}/packapp-x86_64-apple-darwin.zip"
 	sha256 "d254ff9a334741054147d4748d91371d0f392bcdb334c2da6dbad71f8cddf521"
 
--- watch.rb
+++ watch.rb
@@ -4,7 +4,7 @@ class Watch < Formula
 
 	desc "watch files and execute commands"
 	homepage "https://github.com/slmjkdbtl/watch"
-	version "v0.0.0"
+	version "0.0.0"
 	url "https://github.com/slmjkdbtl/watch/releases/download/#{version}/watch-x86_64-apple-darwin.zip"
 	sha256 "f9df5d704b0c67ebc492df3f1faea4866415e90bc821c0e102e2ec77ddf546c7"
 
