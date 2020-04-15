41488ad6edd007c2fc1f0fdb6224a7811dfa6e98
Tiga Wu <tga@wengwengweng.me>
2020-04-15 00:58:32 -0400

    update watch sha256

--- watch.rb
+++ watch.rb
@@ -6,7 +6,7 @@ class Watch < Formula
 	homepage "https://github.com/slmjkdbtl/watch"
 	version "0.0.0"
 	url "https://github.com/slmjkdbtl/watch/releases/download/#{version}/watch-x86_64-apple-darwin.zip"
-	sha256 "ab0bf85278bf8d7ef5d3d2512987f480e741502e33cc4f60399f75e138606796"
+	sha256 "08ce847066f1298a7d86271361d15153d646150176fefb53f77eefd88d7d3be9"
 
 	def install
 		bin.install "watch"
