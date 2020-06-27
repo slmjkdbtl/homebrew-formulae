0b6ec95ee2a2be111e8cd23835ffded89bf5863a
tga <tga@enemyspy.xyz>
2020-06-27 13:53:06 -0400

    update packapp

--- packapp.rb
+++ packapp.rb
@@ -3,10 +3,10 @@
 class Packapp < Formula
 
 	desc "pack a binary to MacOS .app bundle"
-	homepage "https://github.com/slmjkdbtl/packapp"
-	version "0.0.0"
+	homepage "https://git.sr.ht/~slmjkdbtl/packapp"
+	version "0.4.0"
 	url "https://github.com/slmjkdbtl/packapp/releases/download/#{version}/packapp-x86_64-apple-darwin.zip"
-	sha256 "d254ff9a334741054147d4748d91371d0f392bcdb334c2da6dbad71f8cddf521"
+	sha256 "d836b60e3ffa0e9faa401ce153eb84c324ae9a8547015b46dbe85db25e35e5bb"
 
 	def install
 		bin.install "packapp"
